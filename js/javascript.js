b6782fb32f3fb7c76840691b315c3354daeb8aca
Anderson Ponce <nerudasho@gmail.com>
2022-02-23 22:16:47 -0200

    el comienzo

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+let boton = document.querySelector(".boton");
+let text = document.querySelector(".color");
+
+boton.addEventListener('click', ()=>{
+  document.body.style.backgroundColor = "#efeddc";
+document.body.style.color =  "black";
+
+})
