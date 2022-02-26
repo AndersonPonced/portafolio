aec1578a31d9651f7f1069003e1874a0217d29ea
Anderson Ponce <nerudasho@gmail.com>
2022-02-25 23:44:23 -0200

    actualizado

---
+++ js/alert.js
@@ -0,0 +1,21 @@
+const alerta = document.querySelector(".lumiere");
+const buena = document.querySelector(".buena");
+const vene = document.querySelector(".vene");
+
+
+
+alerta.addEventListener('click', () =>{
+    alert("Esta fuera de servicio")
+})
+
+buena.addEventListener('click', () =>{
+    alert("Esta fuera de servicio")
+})
+
+vene.addEventListener('click', () =>{
+    alert("Esta fuera de servicio")
+})
+
+
+
+
