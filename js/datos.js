ee9c549b14a6561f268e32b1d8b51b04b70487d0
Anderson Ponce <nerudasho@gmail.com>
2022-03-05 00:15:59 -0200

    Actualizado: 05/03/22

--- js/datos.js
+++ js/datos.js
@@ -14,3 +14,5 @@ const content = document.querySelectorAll('[data-content]')
     })
 
  })
+
+
--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,30 @@
 let boton = document.querySelector(".boton");
-let text = document.querySelector(".color");
+let letra = document.querySelector(".color");
+let yo = document.querySelector(".yo");
+let boton2 = document.querySelector(".boton2");
 
 boton.addEventListener('click', ()=>{
   document.body.style.backgroundColor = "#efeddc";
-document.body.style.color =  "black";
+  document.body.style.color = "black";
+
+  letra.classList.add("color1");
+  boton.classList.replace("boton", "boton1-opaco");
+
+  boton2.classList.replace("boton2", "boton2-bien")
+  boton2.addEventListener('click', ()=>{
+    
+    document.body.style.backgroundColor = "rgba(15, 23, 30, 0.9)";
+    document.body.style.color = "white";
+
+    letra.classList.replace("color1", "color");
+    boton.classList.replace("boton1-opaco","boton" );
+    boton2.classList.replace("boton2-bien","boton2");
+
+  
+
+  })
+
+   
+
 
 })
