b20e8d019bf41d9e02981d3ed58ca3d8f27fa1bf
Anderson Ponce <nerudasho@gmail.com>
2023-01-27 15:49:43 -0300

    back

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mysql = require('mysql')
 const myconn = require('express-myconnection')
 const cors = require('cors')
+
 const router = require('./routes/router')
 
 const app = express();
